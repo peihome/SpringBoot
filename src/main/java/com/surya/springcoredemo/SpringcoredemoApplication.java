9cf7739dc169c510902e6c807e305bb3687851d7
peihome <ssuurryyaa101@gmail.com>
2024-01-06 17:27:08 -0500

    02-component-scanning

---
+++ src/main/java/com/surya/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,20 @@
+package com.surya.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+@SpringBootApplication(
+		scanBasePackages = {"com.surya.springcoredemo",
+		"com.surya.util"}
+)
+*/
+
+@SpringBootApplication
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+
+}
