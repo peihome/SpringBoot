b43c03f267c7fee766e4e50d20c19c8a369835d7
peihome <ssuurryyaa101@gmail.com>
2024-01-06 17:23:10 -0500

    03-setter-injection

---
+++ src/main/java/com/surya/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,12 @@
+package com.surya.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 20 minutes!!!";
+    }
+}
